4dfb1e5262bf6b5df34fac805790dce6270e5aaa
iRajapaksha <nayanajith.ishara2541@gmail.com>
2024-03-09 19:25:58 +0530

    About us page

--- lib/components/nav_bar.dart
+++ lib/components/nav_bar.dart
@@ -11,9 +11,9 @@ Drawer endDrawer(BuildContext context) {
       children: [
         DrawerHeader(
             child: Image.asset(
-              "assets/icons/PhotoApp_Logo-removebg-preview.png",
-              fit: BoxFit.contain,
-            )),
+          "assets/icons/PhotoApp_Logo-removebg-preview.png",
+          fit: BoxFit.contain,
+        )),
         GestureDetector(
           onTap: () {
             Navigator.push(
@@ -28,15 +28,31 @@ Drawer endDrawer(BuildContext context) {
             title: Text("Home Page"),
           ),
         ),
-        const ListTile(
-          leading: Icon(Icons.info_rounded),
-          title: Text("About"),
+        GestureDetector(
+          onTap: () {
+            showAboutDialog(
+                context: context,
+                applicationIcon: Image.asset(
+                  "assets/icons/PhotoApp_Logo-removebg-preview.png",
+                  width: 80,
+                  height: 80,
+                ),
+                applicationName: 'PhotoApp',
+                applicationVersion: '0.0.1',
+              //  applicationLegalese: 'Developed by faculty of Engineering university of Ruhuna',
+                children: [
+                  AboutUs()
+                ]);
+          },
+          child: const ListTile(
+            leading: Icon(Icons.info_rounded),
+            title: Text("About"),
+          ),
         ),
         GestureDetector(
           onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context)=> const Options()));
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Options()));
           },
           child: const ListTile(
             leading: Icon(Icons.settings),
@@ -50,4 +66,15 @@ Drawer endDrawer(BuildContext context) {
       ],
     ),
   );
+
+  
 }
+Widget AboutUs(){
+    return const Text(
+    'Welcome to Photo App, crafted by computer engineering students at the University of Ruhuna. We''ve designed this app using Flutter to simplify your photo management experience. Whether removing defected, blurred, or similar photos and generating suitable captions for photos in your gallery, Photo App has you covered.\n\n'
+            'Our commitment lies in user satisfaction and technological excellence. We hope you enjoy using Photo App as much as we enjoyed creating it.\n\n'
+            'Thank you for choosing Photo App!\n\n'
+            'The Photo App Team\nUniversity of Ruhuna, Faculty of Engineering',
+            style: TextStyle(fontSize: 16.0),
+    );
+  }
